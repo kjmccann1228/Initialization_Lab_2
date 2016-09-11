f1dd41ccc22bb44affd35a9995b4cf34a3a55885
Kurt McCann <kurtmccann@MacBook-Pro-20.local>
2016-09-11 17:19:09 -0400

    added part 2

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -15,7 +15,7 @@ public class Tester {
         ColorfulThing foo5 = new ColorfulThing(ColorfulThing.Color.SILVER);
         ColorfulThing foo6 = new ColorfulThing(ColorfulThing.Color.GOLD);
         ColorfulThing foo7 = new ColorfulThing(ColorfulThing.Color.GOLD);
-        ColorfulThing foo8 = new ColorfulThing(ColorfulThing.Color.GOLD);
+        ColorfulThing foo8 = new ColorfulThing(ColorfulThing.Color.BRONZE);
         ColorfulThing foo9 = new ColorfulThing(ColorfulThing.Color.GOLD);
 
         testTC.add(foo1);
@@ -28,14 +28,22 @@ public class Tester {
         testTC.add(foo8);
         testTC.add(foo9);
 
-        ColorfulThing foo10 = new ColorfulThing(ColorfulThing.Color.BRONZE);
-        testTC.add(foo10);
+//        ColorfulThing foo10 = new ColorfulThing(ColorfulThing.Color.BRONZE);
+//        testTC.add(foo10);
+//        ColorfulThing removed = testTC.remove(ColorfulThing.Color.SILVER);
+//        System.out.println("I sure hope this say SILVER!" + " ......." + removed.getColor());
 
-//        System.out.println(foo1.getColor());
-//        ColorfulThing foo11 = new ColorfulThing(ColorfulThing.Color.SILVER);
-//        testTC.add(foo11);
-//
+        ColorfulThing removed2 = testTC.remove(foo8);
+        System.out.println("Please be bronze....." + removed2.getColor());
+
+
+//        ColorfulThing theGoldest = testTC.pop();
+//        System.out.println("The goldest is a brilliant " + theGoldest.getColor());
+
         testTC.printTC();
+
+
+
 
 
 
--- src/main/java/ThingContainer.java
+++ src/main/java/ThingContainer.java
@@ -2,8 +2,11 @@
  * Created by kurtmccann on 9/11/16.
  */
 public class ThingContainer {
+
+   //Member variables
     ColorfulThing[] ctArr;                              //ASK TARIQ 1     //Changed1
 //    ColorfulThing[] ctArr = new ColorfulThing[size];    //Change1
+    int nextNullIndex = 0;
 
     //CONSTRUCTOR
     public ThingContainer(int size)
@@ -11,6 +14,7 @@ public class ThingContainer {
       this.ctArr = new ColorfulThing[size];           // ASK TARIQ 1
 //        this.size = size;                           //Change1
     }
+    
 
     public boolean checkIfIndexNull(int index)
     {
@@ -26,12 +30,12 @@ public class ThingContainer {
 
     public void add(ColorfulThing ct)
     {
-        boolean caw = false;
         for(int i = 0; i < this.ctArr.length ; i++)
         {
             if(this.checkIfIndexNull(i))
             {
                 ctArr[i] = ct;
+                nextNullIndex++;
                 return;
             }
         }
@@ -43,9 +47,86 @@ public class ThingContainer {
     {
         for(int i = 0; i<this.ctArr.length; i++)
         {
-            System.out.println("Index " + i + " contains " + ctArr[i].getColor());
+            if(ctArr[i] == null)
+            {
+                System.out.println("Index " + i + " contains NULL");
+            }
+            else
+            {
+                System.out.println("Index " + i + " contains " + ctArr[i].getColor());
+            }
         }
     }
 
+    public ColorfulThing pop()
+    {
+        ColorfulThing popped;
+        for(int i=0 ; i < ctArr.length ; i++)
+        {
+            if(ctArr[i] == null)
+            {
+                popped = this.ctArr[i-1];
+                this.ctArr[i - 1] = null;
+                nextNullIndex--;
+                return popped;
+            }
+        }
+        popped = this.ctArr[ctArr.length-1];
+        this.ctArr[ctArr.length - 1] = null;
+        return popped;
+    }
 
+    public ColorfulThing remove(ColorfulThing.Color col)
+    {
+        ColorfulThing toRemove = null;
+//        System.out.println("TO REMOVE SHOULD BE NULL.  IT IS: " + toRemove);
+        for(int i=0 ; i < ctArr.length ; i++)
+        {
+            if(ctArr[i].getColor().equals(col))
+            {
+                toRemove = ctArr[i];
+                ctArr[i] = null;
+//                nextNullIndex--;
+                consolidate(i);
+                return toRemove;
+            }
+        }
+        return toRemove;
+    }
+
+    public ColorfulThing remove(ColorfulThing thing)
+    {
+        ColorfulThing toRemove = null;
+        for(int i=0 ; i < ctArr.length ; i++)
+        {
+            if(ctArr[i].equals(thing))
+            {
+                toRemove = ctArr[i];
+                ctArr[i] = null;
+//                nextNullIndex--;
+                consolidate(i);
+                return toRemove;
+            }
+        }
+        return toRemove;
+    }
+
+    public void consolidate(int gapIndex)
+    {
+        if(nextNullIndex == ctArr.length) /* Array is full */
+        {
+            for(int i = gapIndex ; i < ctArr.length -1; i++)
+            {
+                ctArr[i] = ctArr[i+1];
+                ctArr[ctArr.length-1] = null;
+            }
+        }
+        else
+        {
+            for (int i = gapIndex ; i < nextNullIndex +1 ; i++)
+            {
+                ctArr[i] = ctArr[i+1];
+            }
+        }
+    }
 }
